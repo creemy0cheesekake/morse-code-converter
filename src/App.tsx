3d2b6f957d8a595309b0fb745bb51b8646791b97
creemy0cheesekake <arjunanand1405@gmail.com>
2022-06-16 22:34:18 -0500

    mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.sass";
+import { ReactComponent as Switch } from "./switch.svg";
+import { engToMorse, morseToEng } from "./translations";
 
-function App() {
+const App = () => {
+  const [eToM, setEToM] = useState(true);
+
+  const [box1, setBox1] = useState("");
+  const [box2, setBox2] = useState("");
+
+  const handleSwitch = () => {
+    setEToM(!eToM);
+    setBox1(box2.toUpperCase());
+    setBox2(box1.toUpperCase());
+  };
+
+  const morseToEnglish = (e: any) => {
+    const english = e.target.value;
+    setBox1(english);
+    const morseArr: any = english
+      .trim()
+      .split("")
+      // @ts-ignore
+      .map((char: string) => engToMorse[char.toUpperCase()]);
+    setBox2(
+      morseArr.every((e: string) => e)
+        ? morseArr.join(" ")
+        : "Error: invalid characters"
+    );
+  };
+
+  const englishToMorse = (e: any) => {
+    const morse = e.target.value;
+    setBox1(morse);
+    const engArr: any = morse
+      .trim()
+      .split(" ")
+      // @ts-ignore
+      .map((char: string) => morseToEng[char.toUpperCase()]);
+    setBox2(
+      engArr.every((e: string) => e)
+        ? engArr.join("")
+        : "Error: invalid Morse code"
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Morse Code Converter</h1>
+      <div className="switch">
+        <p>{eToM ? "English to Morse" : "Morse to English"}</p>
+        <button>
+          <Switch onClick={handleSwitch} />
+        </button>
+      </div>
+      <div className="textareas">
+        <textarea
+          onChange={eToM ? morseToEnglish : englishToMorse}
+          value={box1}
+          cols={30}
+          rows={10}
+        ></textarea>
+        <textarea
+          disabled
+          onChange={eToM ? englishToMorse : morseToEnglish}
+          value={box2}
+          cols={30}
+          rows={10}
+          readOnly={true}
+        ></textarea>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
